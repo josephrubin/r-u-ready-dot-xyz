35a8e97bd7a3dca6b0e02b9e247e8918d3674682
josephrubin <josephmeirrubin@gmail.com>
2018-04-19 22:30:56 +0300

    solved level 1

--- Level_1/Solution/xor_decoder.py
+++ Level_1/Solution/xor_decoder.py
@@ -4,7 +4,7 @@ def main():
     key = "010010010111001101110010011000010110010101101100001011010110100101110011001011010011011100110000"
 
     # Now we use XOR to decode the right BF code by repeating the key as much as necessary
-    ciphertext = open('ciphertext_with_whitespace.txt', 'r')
+    ciphertext = open('xor_ciphertext.txt', 'r')
     plaintext = []
     for line in ciphertext:
         for i, char in enumerate(line):
